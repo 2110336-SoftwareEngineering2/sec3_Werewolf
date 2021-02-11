c006e942d94e0158c9e890ebb945862b8ffddd00
Terash <terash2000@gmail.com>
2021-02-11 21:36:20 +0700

    create user

--- maid-care-service-backend/src/auth/auth.controller.ts
+++ maid-care-service-backend/src/auth/auth.controller.ts
@@ -1,20 +1,27 @@
-import { Controller, Request, Get, Post, UseGuards, Param } from '@nestjs/common';
+import { Controller, Request, Body, Get, Post, UseGuards, Param } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-	
+  constructor(private readonly authService: AuthService, private readonly userService: UsersService ) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return req.user;
   }
-  
+
   @Get('send-verification/:email')
   public async sendEmailVerification(@Param() params): Promise<boolean> {
     var isEmailSent = await this.authService.sendEmailVerification(params.email);
     return isEmailSent;
+  }
+
+  @Post('create-user')
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<boolean> {
+    return await this.userService.createNewUser(createUserDto);
   }
 }
--- maid-care-service-backend/src/users/users.service.ts
+++ maid-care-service-backend/src/users/users.service.ts
@@ -17,4 +17,27 @@ export class UsersService {
   async findOne(email: string): Promise<User> {
     return this.userModel.findOne({email: email}).exec();
   }
+
+  async createNewUser(newUser: CreateUserDto): Promise<boolean> {
+    if(this.isValidEmail(newUser.email) && newUser.password){
+      var userRegistered = await this.findOne(newUser.email);
+      if(!userRegistered){
+        newUser.password = await bcrypt.hash(newUser.password, saltRounds);
+        var createdUser = new this.userModel(newUser);
+        await createdUser.save();
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  isValidEmail (email : string){
+    if(email){
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    } else return false
+  }
 }
