133d4bacdcbd1880812c30a5bac7fe4dab607601
amnuntita <54410295+amnuntita@users.noreply.github.com>
2021-02-12 15:11:18 +0700

    change spacing to unit
    change spacing from pixel to unit (spacing in theme.js is from TailwindCSS default spacing, feel free to fix)

--- maid-care-service-frontend/src/components/pages/login.tsx
+++ maid-care-service-frontend/src/components/pages/login.tsx
@@ -23,36 +23,36 @@ export const LogIn = () => {
   return (
     <Flex bg="brandGreen" align="center" justify="center" minH="100vh">
       <FlexBox>
-        <VStack spacing="20x">
-          <Box fontSize="3xl" mb="30px">
+        <VStack spacing="3">
+          <Box fontSize="3xl" mb="5">
             Grab
             <br />
             Maidcare
           </Box>
           
-          <VStack spacing="20px">
+          <VStack spacing="4">
           <FormControl id="email">
             <FormLabel mb="0">Email address</FormLabel>
             <Input type="email" placeholder="email address" className="formField"/>
           </FormControl>
 
-          <FormControl id="password" width={{sm:"270px",md:"368px"}}>
+          <FormControl id="password" width={{sm:"72",md:"96"}}>
             <FormLabel mb="0">Password</FormLabel>
             <InputGroup>
             <Input type={showPW? "text":"password"} placeholder="password" className="formField"/>
-            <InputRightElement width="4rem">
+            <InputRightElement width="16">
               <Link color="gray.500" fontWeight="700" onClick={() => {setShowPW(!showPW)}} >{showPW? "hide":"show"}</Link>
             </InputRightElement>
             </InputGroup>
           </FormControl>
           </VStack>
           <Center>
-            <Button boxShadow="xl" className="button" mt="25px" mb="10px" bg="buttonGreen">
+            <Button boxShadow="xl" className="button" mt="6" mb="0.75" bg="buttonGreen">
               Log In
             </Button>
           </Center>
           <Link fontSize={{base:"sm",md:"md"}}>Forgot password</Link>
-          <Text fontSize={{base:"sm",md:"md"}} mb="60px">Want to be maid? {" "}
+          <Text fontSize={{base:"sm",md:"md"}} mb="16">Want to be maid? {" "}
             <Link color="#38A169">Create Account Here</Link>
           </Text>
         </VStack>
