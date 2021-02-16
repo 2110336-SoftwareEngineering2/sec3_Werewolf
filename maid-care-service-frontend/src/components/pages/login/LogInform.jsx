d9f31f84b7dca6087de314c4a7ef6b756f8d5f89
amnuntita <54410295+amnuntita@users.noreply.github.com>
2021-02-16 22:14:59 +0700

    axios POST request
    log in request & error message

--- maid-care-service-frontend/src/components/pages/login/LogInform.jsx
+++ maid-care-service-frontend/src/components/pages/login/LogInform.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+  import React, { useState } from "react";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
 
-import { Flex, VStack, Link, Center, Button } from "@chakra-ui/react";
-
+import { Flex, VStack, Link, Center, Button,Box } from "@chakra-ui/react";
 import { TextInput } from "../../shared/FormikField.jsx";
 
 const LogInForm = () => {
+
   const [showPW, setShowPW] = useState(false);
+  const [message,setMessage] = useState(null);
+
   const showPWButton = (
     <Link
       color="gray.500"
@@ -20,20 +23,43 @@ const LogInForm = () => {
     </Link>
   );
 
+  const auth = axios.create({
+    baseURL: "/auth", // use proxy for baseURL
+    headers: {'Content-Type':'application/json'}
+  })
+
+  const yupValidation = Yup.object({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Required"),
+    password: Yup.string()
+  })
+
+  const handleSubmit = (values,{setSubmitting}) => {
+    setTimeout(() => {
+      auth.post('/login',values)
+    .then(response => {
+      setMessage(null);
+      console.log(response.data);
+    })
+    .catch(err => {
+      if(err.response){
+        setMessage(err.response.data.message);
+      }
+      else{
+        setMessage(err.request)
+      }
+    })
+    setSubmitting(false);
+    }, 400);
+  }
 
   return (
     <VStack spacing={4}>
       <Formik
         initialValues={{ email: "", password: "" }}
-        validationSchema={Yup.object({
-          email: Yup.string()
-            .email("Invalid email address")
-            .required("Required"),
-          password: Yup.string()
-        })}
-        onSubmit={values => {
-          console.log(values);
-        }}
+        validationSchema={yupValidation}
+        onSubmit={handleSubmit}
       >
         <Form>
         <Flex flexDirection="column" width={{ sm: "72", md: "96" }}>
@@ -51,6 +77,7 @@ const LogInForm = () => {
               child={showPWButton}
             />
           </Flex>
+          <Box color="red" mt="3">{message}</Box>
           <Center>
           <Button
           boxShadow="xl"
