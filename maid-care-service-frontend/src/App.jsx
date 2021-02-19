d0cb44eb0670a908645cbe8961ecc2267bfee43f
LeeNasriAlone <leealone99@gmail.com>
2021-02-19 21:27:51 +0700

    Add google map window

--- maid-care-service-frontend/src/App.jsx
+++ maid-care-service-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import theme from './theme.js';
 
 import LogIn from './components/pages/login/login.jsx';
 import Home from './components/pages/home/home.jsx';
+import Workspace from './components/pages/workspace/workspace.jsx';
 import ProtectedRoute from './components/protectedRoute';
 
 export const App = () => {
@@ -13,6 +14,7 @@ export const App = () => {
     <ChakraProvider theme={theme}>
       <Switch>
         <Route exact path="/login" component={LogIn} />
+        <Route exact path="/workspace" component={Workspace} />
         <ProtectedRoute exact path="/home" component={Home} />
       </Switch>
     </ChakraProvider>
