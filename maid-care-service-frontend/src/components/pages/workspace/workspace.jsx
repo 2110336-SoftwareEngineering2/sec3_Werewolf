b00b82b786b7807fd452b5e16b19b1abf28ad039
LeeNasriAlone <leealone99@gmail.com>
2021-02-20 19:29:22 +0700

    Add search box, suggestLocation and panTo function

--- maid-care-service-frontend/src/components/pages/workspace/workspace.jsx
+++ maid-care-service-frontend/src/components/pages/workspace/workspace.jsx
@@ -1,10 +1,30 @@
+/*
+    To test this system, you will need
+        - Google map api keys
+            - enable Geocoding API for convert between addresses and geographic coordinates.
+            - enable Maps JavaScript API 
+            - enable Places API for recommended places based on the keywords you enter.
+*/ 
+
 import React, {useState, useCallback, useRef} from "react";
+
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption,
+    ComboboxOptionText,
+} from "@reach/combobox";
+  
 import {
   GoogleMap,
   useLoadScript,
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
+
+// this package is for relocating when user enters the location in the search box
 import usePlacesAutocomplete, {
     getGeocode,
     getLatLng,
@@ -22,8 +42,8 @@ const defauleCenter = {
 
 // mapContainerStyle is height and width of map window.
 const mapContainerStyle = {
-    height: "80vh",
-    width: "80vw",
+    height: "100vh",
+    width: "100vw",
 };
 
 
@@ -55,12 +75,17 @@ export const Workspace = () => {
     const onMapLoad = React.useCallback( (map) => {
         mapRef.current = map;
     }, []);
+
+    const panTo = React.useCallback( ({lat, lng})  => {
+        mapRef.current.panTo( {lat, lng} );
+        mapRef.current.setZoom(14);
+    })
     
     if (isLoadError) return "Error loading maps";
     if(!isLoaded) return "Loading Mpas";
     return (
-        <>
-            <div><h1>Grap maid (This for header)</h1></div>
+        <div><h1>Grap maid (This for header)</h1>
+            <Search  panTo={panTo}/>
             <GoogleMap
                 id="map"
                 mapContainerStyle={mapContainerStyle}
@@ -84,16 +109,60 @@ export const Workspace = () => {
                         onCloseClick={() => { setSelected(null); }}
                     >
                         <div>
-                            <h2>
-                                new workspace
-                            </h2>
-                            <p>Spotted</p>
+                            <h2>new workspace</h2>
                         </div>
                     </InfoWindow>
                 ) : null}
             </GoogleMap>
-        </>
+        </div>
     );
 }
 
 export default Workspace;
+
+
+function Search( {panTo} ) {
+    const {
+        ready, 
+        value, 
+        suggestions: {status, data}, 
+        setValue, 
+        clearSuggestions} = usePlacesAutocomplete({
+            requestOptions: {
+                location: { lat: () => 13.7563,lng: () => 100.5018 },
+                radius: 200 * 1000,
+            }
+        });
+
+    return (
+        <div className="workspace-search">
+            <Combobox
+                onSelect={ async (address) => {
+                    setValue(address, false);
+                    clearSuggestions();
+                    try {
+                        const results = await getGeocode( {address} );
+                        const { lat, lng } = await getLatLng(results[0]);
+                        panTo( {lat, lng} );
+                    } catch(error) {
+                        console.log("Error");
+                    }
+                }}
+            >
+                <ComboboxInput
+                    placeholder="Search" 
+                    value={value} 
+                    onChange={ (event) => { setValue(event.target.value); } }
+                    disabled ={ !ready }
+                />
+                <ComboboxPopover>
+                    <ComboboxList>
+                        { status === "OK" && data.map( ({id, description}) => (
+                            <ComboboxOption key={id} value={description}/>
+                        ))}
+                    </ComboboxList>
+                </ComboboxPopover>  
+            </Combobox>
+        </div>
+    );
+}
