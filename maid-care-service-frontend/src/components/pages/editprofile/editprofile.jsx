5336307a2707ac01206f0c47ef1c1677a9f284cd
noof3310 <noof3310@gmail.com>
2021-02-23 16:18:45 +0700

    Add EditProfileForm Form to EditProfile page

--- maid-care-service-frontend/src/components/pages/editprofile/editprofile.jsx
+++ maid-care-service-frontend/src/components/pages/editprofile/editprofile.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import {
   Box,
   Flex,
-  Stack,
   VStack,
   Image,
-  Link,
-  Text,
 } from "@chakra-ui/react";
 
 import FlexBox from "../../shared/FlexBox";
 import MaidLogo from "../../../MaidLogo.svg";
+import ProfileForm from "./ProfileForm.jsx";
 
 export const EditProfile = () => {
   
@@ -18,17 +16,16 @@ export const EditProfile = () => {
   return (
     <Flex bg="brandGreen" align="center" justify="center" minH="100vh">
       <FlexBox>
-        <VStack spacing="3">
-          <Stack direction="row">
-            <Image
+        <VStack spacing={4}>
+          <Image
                 width="146px"
                 height="41.48px"
                 objectFit="contain"
                 src={MaidLogo}
                 alt="Grab MaidCare Logo"
             />
-          </Stack>
           <Box fontSize="3xl" fontWeight="bold" mb="5">Edit Your Profile</Box>
+          <ProfileForm/>
         </VStack>
       </FlexBox>
     </Flex>
