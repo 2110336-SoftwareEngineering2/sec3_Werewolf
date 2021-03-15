3608f28a10c27a123e355f66137a9846c0a2e6a8
KoFeeBriAN <coffeetk2017@gmail.com>
2021-03-15 20:20:09 +0700

    fixed: promotion

--- maid-care-service-frontend/src/api/index.js
+++ maid-care-service-frontend/src/api/index.js
@@ -32,9 +32,22 @@ const promotion = axios.create({
   baseURL: '/api/promotion',
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('token').toString()}`,
-    secret: process.env.REACT_APP_SECRET,
   },
 });
 
+promotion.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers['secret'] = process.env.REACT_APP_SECRET;
+    }
+    console.log('interceptor conf', config);
+    return config;
+  },
+  error => {
+    console.log('intercaptor err', error);
+    throw error;
+  }
+);
 export { auth, promotion };
