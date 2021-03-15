042f619b85ac39ff1ab03b4393aa0efaed097a24
Teerawat <sakimetos@gmail.com>
2021-03-15 17:10:52 +0700

    back-end workspace swagger fix

--- maid-care-service-backend/src/workspaces/workspaces.module.ts
+++ maid-care-service-backend/src/workspaces/workspaces.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { WorkspacesService } from './workspaces.service';
 import { WorkspacesController } from './workspaces.controller';
 import { WorkspaceProviders } from './workspaces.providers';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports:[DatabaseModule],
   controllers: [WorkspacesController],
   providers: [WorkspacesService, ...WorkspaceProviders]
 })
