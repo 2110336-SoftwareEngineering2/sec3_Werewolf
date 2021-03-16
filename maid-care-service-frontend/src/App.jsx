90322bf48bc4c25414f83b05dcc23c2b75d317bb
KoFeeBriAN <coffeetk2017@gmail.com>
2021-03-16 19:30:06 +0700

    wip: promotion table

--- maid-care-service-frontend/src/App.jsx
+++ maid-care-service-frontend/src/App.jsx
@@ -23,7 +23,7 @@ export const App = () => {
         <Route path="/promotion" component={Promotion} />
         <Route exact path="/profile/edit" component={EditProfile} />
         <Route exact path="/workspace" component={Workspace} />
-        <ProtectedRoute exact path="/home" component={Home} />
+        <Route exact path="/home" component={Home} />
       </Switch>
     </ChakraProvider>
   );
