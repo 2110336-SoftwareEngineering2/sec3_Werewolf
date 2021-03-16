949d43bea026b6a27cff0908e2bdc15c2cdc7301
KoFeeBriAN <coffeetk2017@gmail.com>
2021-03-17 00:20:44 +0700

    feat: jobs maid ui

--- maid-care-service-frontend/src/components/pages/jobs/jobs.jsx
+++ maid-care-service-frontend/src/components/pages/jobs/jobs.jsx
@@ -1,21 +1,108 @@
 import { Button } from '@chakra-ui/button';
-import { Container, Flex, Heading, HStack, VStack } from '@chakra-ui/layout';
-import React, { useEffect } from 'react';
+import {
+  Box,
+  Center,
+  Container,
+  Flex,
+  Heading,
+  HStack,
+  List,
+  ListItem,
+  VStack,
+  Wrap,
+} from '@chakra-ui/layout';
+import { Modal, ModalCloseButton, ModalContent, ModalOverlay } from '@chakra-ui/modal';
+import { Spinner } from '@chakra-ui/spinner';
+import { observer } from 'mobx-react-lite';
+import React, { useEffect, useState } from 'react';
+import { useStores } from '../../../hooks/use-stores';
 import JobItem from './components/JobItem';
 
-const JobsPage = () => {
-  const jobs = [{ id: 1 }, { id: 2 }, { id: 3 }];
+const JobsPage = observer(() => {
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [currentJob, setCurrentJob] = useState(null);
 
-  useEffect(() => {}, []);
+  const { userStore, jobStore } = useStores();
 
   const handleRefresh = (event) => {
-    // TODO: re-fetch jobs data.
+    // TODO: re-fetch jobs data
     console.log('refresh btn clicked!');
+    fetchJobs();
+  };
+
+  const fetchJobs = () => {
+    setLoading(true);
+    return setTimeout(() => {
+      setJobs([
+        {
+          id: 1,
+          works: [
+            {
+              typeOfWork: 'Work A',
+              description: 'Work for A Task',
+              quantity: 112,
+            },
+          ],
+        },
+        {
+          id: 2,
+          works: [
+            {
+              typeOfWork: 'Work A',
+              description: 'Work for A Task',
+              quantity: 102,
+            },
+            {
+              typeOfWork: 'Work B',
+              description: 'Work for B Task',
+              quantity: 102,
+            },
+          ],
+        },
+        {
+          id: 3,
+          works: [
+            {
+              typeOfWork: 'Work A',
+              description: 'Work for A Task',
+              quantity: 92,
+            },
+          ],
+        },
+      ]);
+      setLoading(false);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  const renderModal = () => {
+    return (
+      <Modal
+        size={`xl`}
+        isCentered
+        onClose={() => setCurrentJob(null)}
+        isOpen={currentJob}
+        motionPreset={`slideInBottom`}
+        scrollBehavior={`outside`}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <Box>
+            <JobItem isExpanded={true} job={currentJob} />
+          </Box>
+        </ModalContent>
+      </Modal>
+    );
   };
 
   return (
     <Flex
       direction="column"
+      minH={`100vh`}
       bgColor="gray.300"
       padding={'20vh'}
       justifyContent="flex-start"
@@ -27,14 +114,25 @@ const JobsPage = () => {
             Refresh
           </Button>
         </HStack>
-        <VStack mt={4} p={3} justifyContent="center">
-          {jobs.map((job) => {
-            return <JobItem key={job.id} />;
-          })}
-        </VStack>
+        <List spacing={6} mt={4} p={3} justifyContent="center">
+          {isLoading ? (
+            <Center>
+              <Spinner size={`xl`} thickness={6} />
+            </Center>
+          ) : (
+            jobs.map((job) => {
+              return (
+                <ListItem onClick={() => setCurrentJob(job)} my={2} minW={`90%`}>
+                  <JobItem key={job.id} job={job} />
+                </ListItem>
+              );
+            })
+          )}
+        </List>
+        {renderModal()}
       </Container>
     </Flex>
   );
-};
+});
 
 export default JobsPage;
