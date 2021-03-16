7af94e30dc5af955332435cfd929b65de6f333a8
Terash <terash2000@gmail.com>
2021-03-17 00:59:58 +0700

    add work's unit

--- maid-care-service-backend/src/job/schemas/job.schema.ts
+++ maid-care-service-backend/src/job/schemas/job.schema.ts
@@ -1,13 +1,21 @@
 import * as mongoose from 'mongoose';
+import { JobState } from '../jobState';
 
 export const JobSchema = new mongoose.Schema({
   customerId: String,
   workplaceId: String,
-  work: [{ typeOfWork: String, description: String, quantity: Number }],
+  work: [
+    {
+      typeOfWork: String,
+      description: String,
+      quantity: Number,
+      unit: { type: String, default: null },
+    },
+  ],
   maidId: { type: String, default: null },
   requestedMaid: [String],
   expiryTime: Date,
-  state: { type: String, default: 'posted' },
+  state: { type: String, default: JobState.posted },
   rating: { type: Number, default: 0 },
   review: { type: String, default: null },
 });
