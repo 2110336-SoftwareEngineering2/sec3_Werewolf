52c5c5a98aa70d2d1e122225150cdf45f2c8eac7
LeeNasriAlone <leealone99@gmail.com>
2021-03-17 21:33:08 +0700

    update

--- maid-care-service-frontend/src/api/index.js
+++ maid-care-service-frontend/src/api/index.js
@@ -78,3 +78,27 @@ promotion.interceptors.request.use(
   }
 );
 export { promotion };
+
+const postjob = axios.create({
+  baseURL: '/api/postjob',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+postjob.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers['secret'] = process.env.REACT_APP_SECRET;
+    }
+    console.log('interceptor conf', config);
+    return config;
+  },
+  error => {
+    console.log('intercaptor err', error);
+    throw error;
+  }
+);
+export { postjob };
