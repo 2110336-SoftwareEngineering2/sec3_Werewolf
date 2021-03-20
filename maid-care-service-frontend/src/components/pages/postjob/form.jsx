58cb737e5e529b509650e18b13f254f385ddc2c1
LeeNasriAlone <leealone99@gmail.com>
2021-03-20 21:54:43 +0700

    Move Page2Page3 from form.jsx to Page2Page3_calculatePrice.jsx

--- maid-care-service-frontend/src/components/pages/postjob/form.jsx
+++ maid-care-service-frontend/src/components/pages/postjob/form.jsx
@@ -8,6 +8,7 @@ import { observer } from 'mobx-react-lite';
 import * as Yup from 'yup';
 import ButtonField from './ButtonField.jsx';
 import Page1_TaskDescription from './Page1_TaskDescription.jsx';
+import Page2Page3_calculatePrice from './Page2Page3_calculatePrice.jsx';
 import {
   Link,
   VStack,
@@ -98,7 +99,7 @@ const PostjobForm = observer(props => {
     if (props.steps === 1) {
       return <Page1_TaskDescription />;
     } else if (props.steps === 2 || props.steps === 3) {
-      return <Page2Page3 steps={props.steps} putResponse={putResponse} />;
+      return <Page2Page3_calculatePrice steps={props.steps} putResponse={putResponse} />;
     }
   };
 
@@ -128,126 +129,3 @@ const PostjobForm = observer(props => {
 
 export default PostjobForm;
 
-
-
-const Page2Page3 = ({ steps, putResponse }) => {
-  // this 4 constanct is only for test.
-  const [isPromoAvailable, setPromoAvailable] = useState(null);
-  const [promoData, setPromoData] = useState();
-
-  const { values } = useFormikContext();
-
-  const getPromotioncodeFromServer = () => {
-    promotion
-      .get(`/${values.promotionCode}`, {
-        timeout: 5000,
-      })
-      .then(response => {
-        setPromoAvailable("true");
-        setPromoData(response.data);
-        console.log(promoData);
-      })
-      .catch(error => {
-        console.error(error);
-        setPromoAvailable("false");
-      });
-  }
-
-  const calTotalWithPromo = () => putResponse.cost - ( putResponse.cost * ( isPromoAvailable === 'true' ?  promoData.discountRate : 0 )/ 100 );
-
-  const promotionBox = () => {
-    if (steps == 2) {
-      return (
-        <FormControl id="clothes" width={{ sm: '270px', md: '368px' }} mt="40px">
-          <TextInputField
-            label="Promotion Code"
-            name="promotionCode"
-            placeholder="Apply Your Promotion Code"
-          />
-          <HStack mt="5px">
-            <Button 
-            onClick={getPromotioncodeFromServer}
-            width="100px"
-            >Verify
-            </Button>
-            <Text 
-              color={isPromoAvailable === 'true' ? 'green' :
-              isPromoAvailable === 'false' ? 'red' : 'white'}
-              >
-              {isPromoAvailable === 'true' ? 'Your promotion code is available' :
-              isPromoAvailable === 'false' ? 'Your promotion code is unavailable' : ''
-              }</Text>
-          </HStack>
-        </FormControl>
-      );
-    } else if (steps == 3) {
-      return (
-        <>
-          <HStack justify="space-between" width="100%" mt="20px">
-            <Text fontFamily="body">Promotion (Discount Rate)</Text>
-            <Text fontFamily="body" fontWeight="bold">
-             {isPromoAvailable === 'true' ?  `${promoData.discountRate}%` : '0%' }
-            </Text>
-          </HStack>
-          <HStack justify="space-between" width="100%">
-            <Text fontFamily="body" fontWeight="bold">
-              Total price
-            </Text>
-            <Text fontFamily="body" fontWeight="bold">
-              {calTotalWithPromo()}
-            </Text>
-          </HStack>
-        </>
-      );
-    }
-    return <div></div>;
-  };
-
-  return (
-    <FormControl mb="20px" id="house-no" width={{ sm: '270px', md: '368px' }}>
-      <Text mb="3px" fontWeight="3px" fontSize="20px" fontFamily="body">
-        Total Price
-      </Text>
-      <HStack justify="space-between" width="100%">
-        <Text fontFamily="body">
-          {values.amountOfDishes == '' || values.isDishes === false ? '0' : values.amountOfDishes}{' '}
-          Dishes
-        </Text>
-        <Text fontFamily="body">
-          {putResponse === undefined ? 'Loading...' : putResponse.work[0].cost}
-        </Text>
-      </HStack>
-      <HStack justify="space-between" width="100%">
-        <Text fontFamily="body">
-          {values.areaOfRooms == '' || values.isRooms === false ? '0' : values.areaOfRooms} Square
-          meters of Rooms
-        </Text>
-        <Text fontFamily="body">
-          {putResponse === undefined ? 'Loading...' : putResponse.work[1].cost}
-        </Text>
-      </HStack>
-      <HStack justify="space-between" width="100%">
-        <Text fontFamily="body">
-          {values.amountOfClothes == '' || values.isClothes === false
-            ? '0'
-            : values.amountOfClothes}{' '}
-          Clothes
-        </Text>
-        <Text fontFamily="body">
-          {putResponse === undefined ? 'Loading...' : putResponse.work[2].cost}
-        </Text>
-      </HStack>
-      <HStack justify="space-between" width="100%">
-        <Text fontFamily="body" fontWeight="bold">
-          Total price
-        </Text>
-        <Text fontFamily="body" fontWeight="bold">
-          {putResponse === undefined
-            ? ''
-            : putResponse.work[0].cost + putResponse.work[1].cost + putResponse.work[2].cost}
-        </Text>
-      </HStack>
-      {promotionBox()}
-    </FormControl>
-  );
-};
