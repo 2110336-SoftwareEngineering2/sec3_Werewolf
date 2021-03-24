8e393c84a93c64d7a1058e6ae036024d960d49a9
amnuntita <54410295+amnuntita@users.noreply.github.com>
2021-03-24 18:32:22 +0700

    show message if comes from verification page
    show message "register successfully" if  location history state is /auth/verify

--- maid-care-service-frontend/src/components/pages/login/login.jsx
+++ maid-care-service-frontend/src/components/pages/login/login.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { Box, Flex, VStack, Link, Text } from '@chakra-ui/react';
-import { Redirect } from 'react-router-dom';
+import { Redirect,useLocation} from 'react-router-dom';
 
 import FlexBox from '../../shared/FlexBox';
 import LogInForm from './LogInform.jsx';
 import { useStores } from '../../../hooks/use-stores';
 
+
 export const LogIn = observer(() => {
   const { userStore } = useStores();
+  const history = useLocation().state.history;
 
   if (userStore.isAuthenticated) {
     return <Redirect to="/" />;
@@ -17,17 +19,20 @@ export const LogIn = observer(() => {
   return (
     <Flex bg="brandGreen" align="center" justify="center" minH="100vh">
       <FlexBox>
-        <VStack spacing="3">
-          <Box fontSize="3xl" mb="5">
-            Grab
-            <br />
-            Maidcare
-          </Box>
-          <LogInForm />
-          <Link fontSize={{ base: 'sm', md: 'md' }}>Forgot password</Link>
-          <Text fontSize={{ base: 'sm', md: 'md' }} mb="16">
-            Want to be maid? <Link color="#38A169">Create Account Here</Link>
-          </Text>
+        <VStack spacing="1">
+          {history == '/auth/verify'? <Text fontSize="large" color="green">Register Succesful!</Text>:null}
+          <VStack spacing="3">
+              <Box fontSize="3xl" mb="5">
+                Grab
+                <br />
+                Maidcare
+              </Box>
+              <LogInForm />
+              <Link fontSize={{ base: 'sm', md: 'md' }}>Forgot password</Link>
+              <Text fontSize={{ base: 'sm', md: 'md' }} mb="16">
+                Want to be maid? <Link href="/register" color="#38A169">Create Account Here</Link>
+              </Text>
+            </VStack>
         </VStack>
       </FlexBox>
     </Flex>
