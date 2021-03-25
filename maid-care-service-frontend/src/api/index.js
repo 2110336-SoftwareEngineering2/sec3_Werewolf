fe5908a609807d08762079d1d8ee6f29b969b597
LeeNasriAlone <leealone99@gmail.com>
2021-03-26 00:01:07 +0700

    Add user API

--- maid-care-service-frontend/src/api/index.js
+++ maid-care-service-frontend/src/api/index.js
@@ -40,3 +40,27 @@ job.interceptors.request.use(
 );
 export { postjob };
 export { job };
+
+const user = axios.create({
+  baseURL: '/api/user',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+job.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers['secret'] = process.env.REACT_APP_SECRET;
+    }
+    console.log('interceptor conf', config);
+    return config;
+  },
+  error => {
+    console.log('intercaptor err', error);
+    throw error;
+  }
+);
+export { user };
