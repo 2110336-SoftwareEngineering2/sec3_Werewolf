b3dc01ad603a62658a31c12aeb84322935a986d3
LeeNasriAlone <leealone99@gmail.com>
2021-03-26 20:51:24 +0700

    Add comment

--- maid-care-service-frontend/src/components/pages/postjob/components/AlertButton.jsx
+++ maid-care-service-frontend/src/components/pages/postjob/components/AlertButton.jsx
@@ -10,13 +10,23 @@ import {
   Alert,
 } from '@chakra-ui/react';
 
-// lbtnName : Left Button Name
-// rbtnName : Right Button Name
+// mainbtnName : main button
+// lbtnName : Left Button Name (Button in the alert dialog)
+// rbtnName : Right Button Name Button in the alert dialog)
 // headerText : header text
 // bodyText : body text
 // lbtnFunction : Function that is performed when Left Button Name is clicked
 // rbtnFunction : Function that is performed when Right Button Name is clicked
-const AlertButton = ({ mainbtnName, mainbtnColor, lbtnName, rbtnName, headerText, bodyText, lbtnFunction, rbtnFunction }) => {
+const AlertButton = ({
+  mainbtnName,
+  mainbtnColor,
+  lbtnName,
+  rbtnName,
+  headerText,
+  bodyText,
+  lbtnFunction,
+  rbtnFunction,
+}) => {
   // This 3 variables is used for submit button.
   const [isOpen, setIsOpen] = React.useState(false);
   const onClose = () => setIsOpen(false);
@@ -24,42 +34,42 @@ const AlertButton = ({ mainbtnName, mainbtnColor, lbtnName, rbtnName, headerText
 
   return (
     <>
-    <Button
-            width="100px"
-            className="button button-register"
-            bg={mainbtnColor}
-            onClick={() => setIsOpen(true)}>
-            {mainbtnName}
-          </Button>
-    <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose} isCentered>
-      <AlertDialogOverlay>
-        <AlertDialogContent>
-          <AlertDialogHeader fontSize="lg" fontWeight="bold">
-            {headerText}
-          </AlertDialogHeader>
-          <AlertDialogBody>{bodyText}</AlertDialogBody>
-          <AlertDialogFooter>
-            <Button
-              ref={cancelRef}
-              onClick={() => {
-                onClose();
-                lbtnFunction();
-              }}>
-              {lbtnName}
-            </Button>
-            <Button
-              colorScheme="green"
-              onClick={() => {
-                onClose();
-                rbtnFunction();
-              }}
-              ml={3}>
-              {rbtnName}
-            </Button>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialogOverlay>
-    </AlertDialog>
+      <Button
+        width="100px"
+        className="button button-register"
+        bg={mainbtnColor}
+        onClick={() => setIsOpen(true)}>
+        {mainbtnName}
+      </Button>
+      <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose} isCentered>
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              {headerText}
+            </AlertDialogHeader>
+            <AlertDialogBody>{bodyText}</AlertDialogBody>
+            <AlertDialogFooter>
+              <Button
+                ref={cancelRef}
+                onClick={() => {
+                  onClose();
+                  lbtnFunction();
+                }}>
+                {lbtnName}
+              </Button>
+              <Button
+                colorScheme="green"
+                onClick={() => {
+                  onClose();
+                  rbtnFunction();
+                }}
+                ml={3}>
+                {rbtnName}
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
     </>
   );
 };
