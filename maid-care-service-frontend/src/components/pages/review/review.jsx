db79b0863c2c5731a94d408863f6bd63bb29c68b
LeeNasriAlone <leealone99@gmail.com>
2021-04-04 00:43:41 +0700

    Add API

--- maid-care-service-frontend/src/components/pages/review/review.jsx
+++ maid-care-service-frontend/src/components/pages/review/review.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Box,
   Text,
@@ -23,16 +23,43 @@ import {
   Heading,
 } from '@chakra-ui/react';
 import PostModal from './components/PostModal.jsx'
+import { job } from './../../../api';
 
 // Re
 
-const Review = () => {
+const Review = ( ) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [myJob, setMyJob] = useState();
+  const jobId = '6064b08bdefaec00408fb4a3';
+
+  const fetchJobById = () => {
+    job
+      .get(`/${jobId}`, {
+        timeout: 5000,
+      })
+      .then(response => {
+        setMyJob(response.data);
+        console.log('get job/ : ', response);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchJobById();
+  }, [])
+
+  const workspaceId = '6056e37ab24470004087a7de';
+  const maidId = '605ca4cbfc41950040c4c1d9';
+
+  console.log('x', myJob);
+
 
   return (
     <>
       <Button onClick={onOpen}>Open Modal</Button>
-      <PostModal isOpen={isOpen} onClose={onClose}/>
+      <PostModal isOpen={isOpen} onClose={onClose} job={myJob}/>
     </>
   );
 };
