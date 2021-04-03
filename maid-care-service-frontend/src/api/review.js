4ed213772bad1782d9534cfbf906b1f861c17561
LeeNasriAlone <leealone99@gmail.com>
2021-04-04 00:44:23 +0700

    Modified review API

--- maid-care-service-frontend/src/api/review.js
+++ maid-care-service-frontend/src/api/review.js
@@ -9,3 +9,19 @@ export const review = axios.create({
     secret: process.env.REACT_APP_SECRET,
   },
 });
+
+review.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers['secret'] = process.env.REACT_APP_SECRET;
+    }
+    console.log('interceptor conf', config);
+    return config;
+  },
+  error => {
+    console.log('intercaptor err', error);
+    throw error;
+  }
+);
