508042a6f43771c1be0c7764561fabc181804e11
Terash <terash2000@gmail.com>
2021-04-04 22:43:44 +0700

    promotion description is optional

--- maid-care-service-backend/src/promotion/schemas/promotion.schema.ts
+++ maid-care-service-backend/src/promotion/schemas/promotion.schema.ts
@@ -3,7 +3,7 @@ import * as mongoose from 'mongoose';
 export const PromotionSchema = new mongoose.Schema({
   code: String,
   creater: String,
-  description: String,
+  description: { type: String, default: '' },
   discountRate: Number,
   availableDate: { type: Date, default: Date.now },
   expiredDate: { type: Date, default: null },
