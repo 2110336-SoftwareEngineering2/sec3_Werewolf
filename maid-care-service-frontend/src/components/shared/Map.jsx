bef77e7932453116420294836b58f70eb95269e2
LeeNasriAlone <leealone99@gmail.com>
2021-04-05 12:09:54 +0700

    Update map size, zoom

--- maid-care-service-frontend/src/components/shared/Map.jsx
+++ maid-care-service-frontend/src/components/shared/Map.jsx
@@ -1,45 +1,21 @@
-/*
-    To test this system, you will need
-        - Google map api keys
-            - enable Geocoding API for convert between addresses and geographic coordinates.
-            - enable Maps JavaScript API 
-            - enable Places API for recommended places based on the keywords you enter.
-*/
-
-import React, { useState } from 'react';
-
-import {
-  Box,
-  Center,
-} from '@chakra-ui/react';
-
+import React from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
-
-// this package is for relocating when user enters the location in the search box
-
 const libraries = ['places'];
 
-// defauleCenter is default position when page is rerendering.
-// default position is latitude and longtitude of Bangkok.
-const defauleCenter = {
-  lat: 13.7563,
-  lng: 100.5018,
-};
-
-// mapContainerStyle is height and width of map window.
-
-export const Map = ( {latitude, longtitude, width, height} ) => {
+export const Map = ({ latitude, longtitude }) => {
   const { isLoaded, isLoadError } = useLoadScript({
-    // add your google maps API Keys.
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
   });
   const mapContainerStyle = {
-    height: height,
-    width: width,
+    height: '100%',
+    width: '100%',
   };
 
-  // markers is variable that contain a map marker icon which created by user when they click on the map.
+  const defauleCenter = {
+    lat: latitude,
+    lng: longtitude,
+  };
 
   const mapRef = React.useRef();
   const onMapLoad = React.useCallback((map) => {
@@ -48,22 +24,21 @@ export const Map = ( {latitude, longtitude, width, height} ) => {
 
   const panTo = React.useCallback(({ lat, lng }) => {
     mapRef.current.panTo({ lat, lng });
-    mapRef.current.setZoom(14);
+    mapRef.current.setZoom(15);
   });
 
   if (isLoadError) return 'Error loading maps';
   if (!isLoaded) return 'Loading Maps';
+  
   return (
-    <Box bg="gray.200" w="100%" h="100%">
-        <GoogleMap
-          id="map"
-          mapContainerStyle={mapContainerStyle}
-          zoom={8}
-          center={defauleCenter}
-          onLoad={onMapLoad}>
-          <Marker position={{ lat: latitude, lng: longtitude }}/>
-        </GoogleMap>
-    </Box>
+    <GoogleMap
+      id="map"
+      mapContainerStyle={mapContainerStyle}
+      zoom={15}
+      center={defauleCenter}
+      onLoad={onMapLoad}>
+      <Marker position={{ lat: latitude, lng: longtitude }} />
+    </GoogleMap>
   );
 };
 
