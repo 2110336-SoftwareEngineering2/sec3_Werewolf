0cae508bcbed4756c1f405107c49c7a5143fa057
LeeNasriAlone <leealone99@gmail.com>
2021-04-05 12:15:21 +0700

    Add note for use map component

--- maid-care-service-frontend/src/components/shared/Map.jsx
+++ maid-care-service-frontend/src/components/shared/Map.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 const libraries = ['places'];
 
+// Note for Map component.
+// - you need to pass latitude and longtitude as a parameter.
+// - size of map is width = 100%, height = 100%, if you want to edit size => you can wrap them with a box tag or whatever and then adjust size of that box tag.
+
+
 export const Map = ({ latitude, longtitude }) => {
   const { isLoaded, isLoadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -29,7 +34,7 @@ export const Map = ({ latitude, longtitude }) => {
 
   if (isLoadError) return 'Error loading maps';
   if (!isLoaded) return 'Loading Maps';
-  
+
   return (
     <GoogleMap
       id="map"
