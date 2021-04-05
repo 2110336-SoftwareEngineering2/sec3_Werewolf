5d3dde28a593c6b5820f215552fd3b622e7df9ef
amnuntita <54410295+amnuntita@users.noreply.github.com>
2021-04-05 20:06:39 +0700

    get filename directly from file.name

--- maid-care-service-frontend/src/store/Image.js
+++ maid-care-service-frontend/src/store/Image.js
@@ -1,12 +1,11 @@
 import { action, makeObservable, observable } from 'mobx';
 
 import firebase from 'firebase/app';
-import storage from '../firebase.js';
-import { observerBatching } from 'mobx-react-lite';
+import storage from '../firebase.js'; 
+
 var storageRef = storage.ref();
 
-
-class ImageStore {
+class MultiImageStore {
   path_list = {};
   isUploading = false;
 
@@ -19,8 +18,8 @@ class ImageStore {
     });
   }
 
-  // upload action 
-  upload(file,filename) {
+  // upload action
+  upload(file) {
     var uploadTask = storageRef.child('images/' + file.name).put(file);
 
     // Listen for state changes, errors, and completion of the upload.
@@ -62,20 +61,111 @@ class ImageStore {
         // Upload completed successfully, now we can get the download URL
         const downloadURL = await uploadTask.snapshot.ref.getDownloadURL();
         this.isUploading = false;
-        this.path_list[filename] = downloadURL;
+        this.path_list[file.name] = downloadURL;
       }
     );
   }
 
-  delete(filename){
-      var imgRef = storageRef.child(`images/${filename}`)
-      
-      imgRef.delete().then(() => {
-        delete this.path_list[filename]
-      })
-      .catch((err) => {console.log(err)})
-  }
+  delete(filename) {
+    var imgRef = storageRef.child(`images/${filename}`);
 
+    imgRef
+      .delete()
+      .then(() => {
+        delete this.path_list[filename];
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
 
-export default ImageStore;
+// store for uploading only 1 image at a time
+
+class SingleImageStore {
+  path = null;
+  filename = null;
+  isUploading = false;
+
+  constructor() {
+    makeObservable(this, {
+      path: observable,
+      isUploading: observable,
+      upload: action,
+      delete: action,
+    });
+  }
+
+  // upload action
+  upload(file) {
+    var uploadTask = storageRef.child('images/' + file.name).put(file);
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(
+      firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            this.isUploading = true;
+            break;
+        }
+      },
+      (error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            break;
+          case 'storage/canceled':
+            // User canceled the upload
+            break;
+
+          // ...
+
+          case 'storage/unknown':
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
+      async () => {
+        // Upload completed successfully, now we can get the download URL
+        const oldName = this.filename;
+        const downloadURL = await uploadTask.snapshot.ref.getDownloadURL();
+
+        //change URL and filename to new img
+        this.isUploading = false;
+        this.path = downloadURL;
+        this.filename = file.name;
+
+        //delete old img from firebase
+        if(oldName){
+            console.log('deleteing', oldName)
+            this.delete(oldName);
+        }
+      }
+    );
+  }
+
+  delete(filename) {
+    var imgRef = storageRef.child(`images/${filename}`)
+
+    imgRef
+      .delete()
+      .then(() => {
+        console.log('deleted');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+export { MultiImageStore, SingleImageStore };
