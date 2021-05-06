029d64b43d7cc9533afb1d3064ea612c0fbf91a3
KoFeeBriAN <coffeetk2017@gmail.com>
2021-05-06 22:11:50 +0700

    feat: docker-compose

---
+++ maid-care-service-frontend/src/firebase-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyBgMrWO5KQ0E7g-6zrOOcWcZVVTO7l50h4',
+  authDomain: 'maidcare-da56d.firebaseapp.com',
+  projectId: 'maidcare-da56d',
+  storageBucket: 'maidcare-da56d.appspot.com',
+  messagingSenderId: '554706491871',
+  appId: '1:554706491871:web:510cbeb0756c4341a84c6a',
+  measurementId: 'G-XWF2TRLZCR',
+};
